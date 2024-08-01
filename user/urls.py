9c1aa01947d806a0665b3fefd5013cbcb455b7a8
Mykola Setrakov <nikset1@gmail.com>
2024-08-01 18:18:40 +0300

    register urls for user

---
+++ user/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+    TokenBlacklistView,
+)
+
+from user import views
+from user.views import CreateUserView
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("logout/", TokenBlacklistView.as_view(), name="token_blacklist"),
+]
+
+app_name = "user"
