d356f1cd9c473bd50762baac8436f46887e1e8e4
Mykola Setrakov <nikset1@gmail.com>
2024-08-01 18:19:14 +0300

    add CommentViewSet with actions

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import User, Comment
+from .serializers import CommentSerializer, CommentAttachmentSerializer, CommentListSerializer
 
-# Create your views here.
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.filter(parent=None)
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return CommentListSerializer
+
+        if self.action == "upload_file":
+            return CommentAttachmentSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    @action(detail=True, methods=["post"])
+    def reply(self, request, pk=None):
+        parent_comment = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save(user=request.user, parent=parent_comment)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=["post"])
+    def upload_file(self, request, pk=None):
+        item = self.get_object()
+        serializer = self.get_serializer(item, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
