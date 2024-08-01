5ee772b0d0cf19adee7acef4256d7e318ddb76c3
Mykola Setrakov <nikset1@gmail.com>
2024-08-01 18:14:45 +0300

    add avatar image file path function and make migrations

--- user/models.py
+++ user/models.py
@@ -1,9 +1,22 @@
+import os
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.text import slugify
+
+
+def avatar_image_file_path(instance: "User", filename: str) -> str:
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.username)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/avatars/", filename)
 
 
 class User(AbstractUser):
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    avatar = models.ImageField(
+        upload_to=avatar_image_file_path, null=True, blank=True
+    )
 
     def __str__(self):
         return self.username
