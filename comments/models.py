d6c439132869b5e95451bd699f5f72927605ccb0
Mykola Setrakov <nikset1@gmail.com>
2024-08-01 18:13:45 +0300

    add attachment field to Comment model and make migrations

--- comments/models.py
+++ comments/models.py
@@ -1,28 +1,39 @@
+import os
+import uuid
+
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.text import slugify
 
 User = get_user_model()
+
+
+def attachment_image_file_path(instance: "Comment", filename: str) -> str:
+    _, extension = os.path.splitext(filename)
+    filename = (
+        f"{slugify(instance.user.username)}-{uuid.uuid4()}{extension}"
+    )
+
+    return os.path.join("uploads/attachments/", filename)
 
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     email = models.EmailField()
     homepage = models.URLField(blank=True, null=True)
+    attachment = models.FileField(upload_to=attachment_image_file_path, blank=True, null=True)
     text = models.TextField()
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="replies",
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ["-created_at"]
 
     def __str__(self):
-        return f'Comment by {self.user.username}'
-
-
-class Attachment(models.Model):
-    comment = models.ForeignKey(Comment, related_name='attachments', on_delete=models.CASCADE)
-    file = models.FileField(upload_to='attachments/')
-    is_image = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f'Attachment for comment {self.comment.id}'
+        return f"Comment by {self.user.username}"
