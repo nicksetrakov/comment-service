2d8aa895d16ee55c461c4ce0f336a99614f44cbf
Mykola Setrakov <nikset1@gmail.com>
2024-08-03 17:22:17 +0300

    add consumers.py

---
+++ comments/consumers.py
@@ -0,0 +1,36 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+class CommentConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = "comments"
+        self.room_group_name = "comments_group"
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name, self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name, self.channel_name
+        )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {"type": "comment_message", "message": message},
+        )
+
+    async def comment_message(self, event):
+        message = event["message"]
+
+        await self.send(text_data=json.dumps({"message": message}))
