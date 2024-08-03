e56ce730da7762f2ccaecf335e7d0d4040535885
Mykola Setrakov <nikset1@gmail.com>
2024-08-03 17:21:44 +0300

    add websocket protocol

--- comment_system/asgi.py
+++ comment_system/asgi.py
@@ -9,8 +9,19 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comment_system.settings')
+import comments
 
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "comment_system.settings")
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(comments.routing.websocket_urlpatterns)
+        ),
+    }
+)
