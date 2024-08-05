817e709d76e6b336595b22bfb12fd0be3e198208
Mykola Setrakov <nikset1@gmail.com>
2024-08-06 01:11:05 +0300

    update imports after setup django

--- comment_system/asgi.py
+++ comment_system/asgi.py
@@ -9,19 +9,22 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-import comments
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "comment_system.settings")
+django.setup()  # Инициализация Django
+
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from comments.middlewares import JWTAuthMiddlewareStack
+from comments.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(comments.routing.websocket_urlpatterns)
-        ),
+        "websocket": JWTAuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
     }
 )
